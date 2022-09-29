2b21ef7821c8d190bd54593073cc2f924ffd9139
Devakumar D <devakumar.dhanapoosanam@syncfusion.com>
2022-09-29 08:35:08 +0530

    Added the chart segment color change sample based on y values.

---
+++ MAUI_Chart_Segment_Color/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUI_Chart_Segment_Color;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MAUI_Chart_Segment_Color/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_Chart_Segment_Color;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
