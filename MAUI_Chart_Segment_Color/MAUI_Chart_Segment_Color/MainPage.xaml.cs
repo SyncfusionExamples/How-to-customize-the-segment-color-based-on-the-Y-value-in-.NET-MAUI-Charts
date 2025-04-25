541b829f977b20b894ef15ab67ce5eb353d85094
Vallarasu4653 <vallarasu.ravichandran@syncfusion.com>
2025-04-25 19:11:55 +0530

    Update latest version sample

---
+++ MAUI_Chart_Segment_Color/MAUI_Chart_Segment_Color/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUI_Chart_Segment_Color
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ MAUI_Chart_Segment_Color/MAUI_Chart_Segment_Color/ViewModel/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MAUI_Chart_Segment_Color
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Model> Data { get; set; }
+
+        public ObservableCollection<Brush> CustomBrushes { get; set; }
+
+        public ViewModel() 
+        {
+            Data =
+            [
+                new Model("2015", 25),
+                new Model("2016", 17),
+                new Model("2017", 20),
+                new Model("2018", 30),
+                new Model("2019", 41),
+                new Model("2020", 35),
+                new Model("2021", 46),
+            ];
+
+            CustomBrushes = new ObservableCollection<Brush>();
+
+            foreach (var item in Data)
+            {
+                if (item.YValue <= 20)
+                {
+                    CustomBrushes.Add(new SolidColorBrush(Colors.CadetBlue));
+                }
+                else if (item.YValue > 20 && item.YValue <= 40)
+                {
+                    CustomBrushes.Add(new SolidColorBrush(Colors.DarkGray));
+                }
+                else if (item.YValue > 40)
+                {
+                    CustomBrushes.Add(new SolidColorBrush(Colors.Orange));
+                }
+            }
+        }
+    }
+}
